24875eb2b7b0cb221dac4a7931ef45efd6feb7d1
Satyarth Prakash Yadav <84712739+satyarth7619@users.noreply.github.com>
2023-07-15 23:49:21 +0530

    .

---
+++ buyAndsell.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class buyAndsell {
+    public static int byuAndSellStock(int prices[]){
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit =0;
+        for(int i =0; i<prices.length; i++){
+            if(buyPrice<prices[i]){
+                int profit =prices[i] - buyPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            }
+            else{
+                buyPrice = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+    public static void main(String args[]) {
+        int prices[] = {7,1,5,3,6,4};
+        System.out.println(byuAndSellStock(prices));
+    }
+}
